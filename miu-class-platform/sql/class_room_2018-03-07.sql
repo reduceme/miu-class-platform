58209a0c01f6f4779be8b88178898a0e269171ca
regdai <1148440350@qq.com>
2018-03-07 17:50:17 +0800

    mysql

---
+++ miu-class-platform/sql/class_room_2018-03-07.sql
@@ -0,0 +1,3 @@
+INSERT INTO `class_room`(`roomId`, `classroom`) VALUES (1, 'Miuyoga瑜伽工作室（光华逸家）');
+INSERT INTO `class_room`(`roomId`, `classroom`) VALUES (2, 'Miuyoga瑜伽工作室（南湖逸家）');
+INSERT INTO `class_room`(`roomId`, `classroom`) VALUES (3, 'Miuyoga瑜伽工作室（南阳锦城）');
---
+++ miu-class-platform/sql/timetable_2018-03-07.sql
@@ -0,0 +1,7 @@
+INSERT INTO `timetable`(`classId`, `roomId`, `time`, `teacher`, `week`, `maxCount`, `minCount`, `classname`, `status`) VALUES (1, 1, '10:00', '毛毛老师', 1, 5, 2, '空中瑜伽', 'true');
+INSERT INTO `timetable`(`classId`, `roomId`, `time`, `teacher`, `week`, `maxCount`, `minCount`, `classname`, `status`) VALUES (2, 1, '10:01', '毛毛老师', 2, 5, 2, '初级瑜伽', 'true');
+INSERT INTO `timetable`(`classId`, `roomId`, `time`, `teacher`, `week`, `maxCount`, `minCount`, `classname`, `status`) VALUES (3, 1, '10:03', '毛毛老师', 3, 5, 2, '高温瑜珈', 'true');
+INSERT INTO `timetable`(`classId`, `roomId`, `time`, `teacher`, `week`, `maxCount`, `minCount`, `classname`, `status`) VALUES (4, 1, '10:04', '毛毛老师', 4, 5, 2, '肩颈理疗', 'true');
+INSERT INTO `timetable`(`classId`, `roomId`, `time`, `teacher`, `week`, `maxCount`, `minCount`, `classname`, `status`) VALUES (5, 1, '10:05', '毛毛老师', 5, 5, 2, '入门瑜伽', 'true');
+INSERT INTO `timetable`(`classId`, `roomId`, `time`, `teacher`, `week`, `maxCount`, `minCount`, `classname`, `status`) VALUES (6, 1, '10:06', '毛毛老师', 6, 5, 2, '流瑜伽', 'true');
+INSERT INTO `timetable`(`classId`, `roomId`, `time`, `teacher`, `week`, `maxCount`, `minCount`, `classname`, `status`) VALUES (8, 1, '19:30', '蛋蛋老师', 3, 5, 2, '拜日式', 'true');
