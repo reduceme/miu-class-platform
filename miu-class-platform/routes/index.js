24e21c5e7ef075a1452795f4d6c553cd4aa20e62
regdai <1148440350@qq.com>
2018-03-19 17:55:49 +0800

    个人中心路由

--- miu-class-platform/routes/index.js
+++ miu-class-platform/routes/index.js
@@ -14,4 +14,11 @@ router.get('/time-table', function (req, res, next) {
     res.render('time-table', {title: '课表'});
 });
 
+router.get('/user-info', function (req, res, next) {
+    if (!req.cookies.user) {
+        return res.render('index', {title: '登录'})
+    }
+    res.render('user-info', {title: '个人中心'});
+});
+
 module.exports = router;
